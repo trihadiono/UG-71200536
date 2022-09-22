a69f0b3d803bd63f702fbfdbdd3793fc4fc810c3
trihadiono <tri.hadiono@ti.ukdw.ac.id>
2022-09-22 18:24:37 +0700

    COMMIT

--- UG4/soal1-71200536/src/main/java/com/ug4/soal1/Main.java
+++ UG4/soal1-71200536/src/main/java/com/ug4/soal1/Main.java
@@ -1,147 +1,132 @@
 package com.ug4.soal1;
+
 import java.util.Scanner;
 
 public class Main {
-    static final Scanner scanner = new Scanner(System.in);
+    static final Scanner scanner;
 
-    public static void main( String[] args )
-    {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
         initiateMenu();
     }
 
-    private static void initiateMenu(){
-
-        Passenger adit = new Passenger("Adit","adit@gmail.com",100000);
-        Passenger surya = new Passenger("Surya","surya@gmail.com",20000);
-        Passenger krismon = new Passenger("Krismon","krismon@gmail.com", 10000);
-        Driver pakTole = new Driver("Pak Tole","pria",35);
-
-
-        Bus dutaBus = new Bus("Duta Bus",pakTole);
-
-        /*
-        masuk pada test case. sistem akan langsung mengarahkan pada method menu()
-         */
+    private static void initiateMenu() {
+        Passenger adit = new Passenger("Adit", "adit@gmail.com", 100000.0);
+        Passenger surya = new Passenger("Surya", "surya@gmail.com", 20000.0);
+        Passenger krismon = new Passenger("Krismon", "krismon@gmail.com", 10000.0);
+        Driver pakTole = new Driver("Pak Tole", "pria", 35);
+        Bus dutaBus = new Bus("Duta Bus", pakTole);
         System.out.println("Duta Bus");
-        //case penumpang adit
-        menu(adit,dutaBus);
-        //case penumpang surya
-        menu(surya,dutaBus);
-        //case penumpang krismon
-        menu(krismon,dutaBus);
-
-        //case telah sampai di tujuan pertama
+        menu(adit, dutaBus);
+        menu(surya, dutaBus);
+        menu(krismon, dutaBus);
         System.out.println("Pemberhentian pertama telah sampai pada Kampus Universitas Kristen Duta Wacana");
-        dutaBus.dropPassengers(dutaBus.getRoute()[0],adit);
-        dutaBus.dropPassengers(dutaBus.getRoute()[0],surya);
-        dutaBus.dropPassengers(dutaBus.getRoute()[0],krismon);
+        dutaBus.dropPassengers(dutaBus.getRoute()[0], adit);
+        dutaBus.dropPassengers(dutaBus.getRoute()[0], surya);
+        dutaBus.dropPassengers(dutaBus.getRoute()[0], krismon);
         System.out.println("Sisa penumpang sebanyak " + dutaBus.getUsedCapacity() + " orang");
         System.out.println("============================================");
         System.out.println();
         System.out.println();
-
-
         System.out.println("Pemberhentian kedua telah sampai pada Asrama Omah Babadan");
-        dutaBus.dropPassengers(dutaBus.getRoute()[1],adit);
-        dutaBus.dropPassengers(dutaBus.getRoute()[1],surya);
-        dutaBus.dropPassengers(dutaBus.getRoute()[1],krismon);
+        dutaBus.dropPassengers(dutaBus.getRoute()[1], adit);
+        dutaBus.dropPassengers(dutaBus.getRoute()[1], surya);
+        dutaBus.dropPassengers(dutaBus.getRoute()[1], krismon);
         System.out.println("Sisa penumpang sebanyak " + dutaBus.getUsedCapacity() + " orang");
         System.out.println("============================================");
         System.out.println();
         System.out.println();
-
-        //case telah sampai di tujuan kedua
         System.out.println("Pemberhentian ketiga telah sampai pada Asrama Teologi");
-        dutaBus.dropPassengers(dutaBus.getRoute()[2],adit);
-        dutaBus.dropPassengers(dutaBus.getRoute()[2],surya);
-        dutaBus.dropPassengers(dutaBus.getRoute()[2],krismon);
+        dutaBus.dropPassengers(dutaBus.getRoute()[2], adit);
+        dutaBus.dropPassengers(dutaBus.getRoute()[2], surya);
+        dutaBus.dropPassengers(dutaBus.getRoute()[2], krismon);
         System.out.println("Sisa penumpang sebanyak " + dutaBus.getUsedCapacity() + " orang");
         System.out.println("============================================");
         System.out.println();
         System.out.println();
-
-        //menampilkan keuntungan yang diperoleh
         System.out.println("Keuntungan hari ini");
-        System.out.println("Keuntungan sebesar : Rp " +dutaBus.getProfit() + "0;-");
+        System.out.println("Keuntungan sebesar : Rp " + dutaBus.getProfit() + "0;-");
     }
 
-    private static void menu(Passenger passenger, Bus bus){
+    private static void menu(Passenger passenger, Bus bus) {
         System.out.println("Selamat Datang Kak " + passenger.getName());
-        /*
-        Lengkapilah method ini agar sesuai dengan ketentuan dan output
-        case : mengecek apakah saldo yang dimiliki penumpang lebih dari tarif perjalanan atau tidak
-        jika tidak, maka akan mengarahkan ke menu top up. Namun jika cukup maka akan mengarahkan
-        ke menampilkan destinasi tujuan
-         */
+        if (bus.checkPassengersBalance(passenger)) {
+            showDestination(bus, passenger);
+        } else {
+            topUpMenu(bus, passenger);
+        }
+
         System.out.println("============================================\n\n\n");
     }
 
-    private static void showDestination(
-            Bus bus,
-            Passenger passenger
-    ){
-        String destiny;
+    private static void showDestination(Bus bus, Passenger passenger) {
         int i = 0;
         System.out.println("Pilihan rute perjalanan");
-        for(String rute : bus.getRoute()){
-            i++;
-            System.out.print(i + ". " + rute + "\n");
+        String[] var4 = bus.getRoute();
+        int var5 = var4.length;
+
+        int var6;
+        String route;
+        for(var6 = 0; var6 < var5; ++var6) {
+            route = var4[var6];
+            ++i;
+            System.out.print(i + ". " + route + "\n");
         }
+
         System.out.print("masukan destinasi perjalanan anda : ");
-        destiny = scanner.nextLine();
-        //mengecek apakah input sudah sesuai atau belum
-        if(destiny.isEmpty()) {
-            /*
-            Lengkapilah method ini agar sesuai dengan ketentuan dan output
-            case: jika tidak sesuai maka akan menampilkan error, kemudian akan memanggil kembali
-            method showDestination()
-             */
-        }
-        else{
-            for(String route: bus.getRoute()){
-                /*
-                Lengkapilah method ini agar sesuai dengan ketentuan dan output
-                case: melakukan pengecekan apakah route yang diinputkan sesuai dengan ketiga rute yang ada.
-                Kika sesuai maka order perjalanan akan diproses (proceed order). Namun jika input tidak sesuai,
-                maka akan menampilkan error message dan akan mengarahkan kembali pada method showDestination()
-                 */
+        String destiny = scanner.nextLine();
+        scanner.reset();
+        if (destiny.isEmpty()) {
+            System.out.println("\nAnda belum memasukan tujuan");
+            showDestination(bus, passenger);
+        } else {
+            var4 = bus.getRoute();
+            var5 = var4.length;
+
+            for(var6 = 0; var6 < var5; ++var6) {
+                route = var4[var6];
+                if (route.equalsIgnoreCase(destiny)) {
+                    bus.proceedOrder(destiny, passenger);
+                    System.out.flush();
+                } else {
+                    System.out.println("Maaf tujuan yang anda masukan tidak sesuai");
+                    showDestination(bus, passenger);
+                }
             }
         }
+
     }
 
-    private static void topUpMenu(
-            Bus bus,
-            Passenger passenger
-    ){
-        String choice;
-        double balance;
+    private static void topUpMenu(Bus bus, Passenger passenger) {
         System.out.println("Anda ingin top up ?");
         System.out.print("jawaban anda (ya/tidak): ");
-        choice = scanner.nextLine();
-        //melakukan pengecekan pada input pilihan
-        if(choice.equalsIgnoreCase("ya")) {
+        String choice = scanner.nextLine();
+        if (choice.equalsIgnoreCase("ya")) {
             System.out.print("Masukan nominal top up : ");
-            /*
-            Lengkapilah method ini agar sesuai dengan ketentuan dan output
-            case: melakukan pengecekan apakah input balance kosong atau tidak. Jika kosong maka
-            akan menampilkan error message dan kembali memanggil method topUpMenu(). Namun jika
-            input tidak kosong maka akan memproses top up saldo dan akan mengarahkan ke method
-            showDestination()
-             */
-        }
-        else if(choice.equalsIgnoreCase("tidak")){
-            /*
-            Lengkapilah method ini agar sesuai dengan ketentuan dan output
-            case: jika input menunjukan tidak, maka akan membatalkan order perjalanan
-             */
-        }
-        else{
-            /*
-            Lengkapilah method ini agar sesuai dengan ketentuan dan output
-            case: jika input bukan ya/tidak, maka akan menampilkan error message dan
-            akan kembali memanggil method topUpMenu();
-             */
+            double balance = scanner.nextDouble();
+            if ((balance + "").isEmpty()) {
+                System.out.println("Kamu belum memasukan nominal top up");
+                System.out.println("\n\n");
+                topUpMenu(bus, passenger);
+            } else {
+                bus.topUpBalance(balance, passenger);
+                scanner.nextLine();
+                System.out.flush();
+                showDestination(bus, passenger);
+            }
+        } else if (choice.equalsIgnoreCase("tidak")) {
+            bus.cancelOrder(passenger);
+        } else {
+            System.out.println("Input yang anda masukan salah");
+            System.out.println("\n\n");
+            topUpMenu(bus, passenger);
         }
 
+    }
+
+    static {
+        scanner = new Scanner(System.in);
     }
 }

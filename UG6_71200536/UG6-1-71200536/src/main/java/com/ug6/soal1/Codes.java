4eef86cc8484195351bd28baaa8b647046f27f75
trihadiono <tri.hadiono@ti.ukdw.ac.id>
2022-10-06 18:36:01 +0700

    COMMIT

--- UG6_71200536/UG6-1-71200536/src/main/java/com/ug6/soal1/Codes.java
+++ UG6_71200536/UG6-1-71200536/src/main/java/com/ug6/soal1/Codes.java
@@ -1,4 +1,23 @@
 package com.ug6.soal1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Codes {
+    private static String GOODSCODES="GD";
+    private static String VENDINGMACHINESCODE="VM";
+    private final String[] TYPE={"DR","FD","DG"};
+
+    public String generateGoodsCode(String type, String name){
+        int len=name.length();
+        String contain=GOODSCODES+type+name.charAt(0)+name.charAt(len-1);
+        return contain;
+    }
+    public String generateVendingMachineCode(int order){
+        if (order>9){
+            return VENDINGMACHINESCODE+order;
+        }else{
+            return VENDINGMACHINESCODE+"0"+order;
+        }
+    }
 }

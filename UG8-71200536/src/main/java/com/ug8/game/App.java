f150921202fb6499866f002f8abb806a87cdb4f5
trihadiono <tri.hadiono@ti.ukdw.ac.id>
2022-11-03 18:30:01 +0700

    COMMITT

--- UG8-71200536/src/main/java/com/ug8/game/App.java
+++ UG8-71200536/src/main/java/com/ug8/game/App.java
@@ -1,13 +1,9 @@
 package com.ug8.game;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }

fffa57e5e329af9d4d4d7d3b2b4e0b4da6b91d43
trihadiono <78341929+trihadiono@users.noreply.github.com>
2022-11-17 17:06:06 +0700

    COMMITT

---
+++ UG10A_71200536/src/main/java/com/ug10/register/App.java
@@ -0,0 +1,13 @@
+package com.ug10.register;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ UG10A_71200536/src/main/java/com/ug10/register/User.java
@@ -0,0 +1,11 @@
+package com.ug10.register;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public User (String username, String email, String password){
+        
+    }
+}

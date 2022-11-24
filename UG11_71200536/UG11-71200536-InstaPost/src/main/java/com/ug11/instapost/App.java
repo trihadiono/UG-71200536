4128309232ed65e911107ac05deda8de124d571f
trihadiono <78341929+trihadiono@users.noreply.github.com>
2022-11-24 16:52:58 +0700

    COMMIT

---
+++ UG11_71200536/UG11-71200536-InstaPost/src/main/java/com/ug11/instapost/App.java
@@ -0,0 +1,13 @@
+package com.ug11.instapost;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
